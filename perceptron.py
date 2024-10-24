a0f5a24e5941d59fbc94cfc7ebafb105dc64bf94
Yan Min <yc89@hw.ac.uk>
2024-10-24 17:28:07 +0100

    Implemented activation function

--- perceptron.py
+++ perceptron.py
@@ -8,6 +8,6 @@ class Perceptron:
     def __init__(self,activationFunc):
         self.activationFunc = activationFunc
 
-# YM: I think we haven't implement the activation func right?
     def CalculateOutput(self, weights, input):
-        return np.dot(np.array(input).T,np.array(weights))
+        z = np.dot(np.array(input).T,np.array(weights))
+        return self.activationFunc(z)
