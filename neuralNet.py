11d6911d057d1824f87b5f7cbb965ec83fc94cf5
Yan Min <yc89@hw.ac.uk>
2024-11-12 10:53:37 +0000

    Added sse calculation

--- neuralNet.py
+++ neuralNet.py
@@ -8,9 +8,14 @@ class neuralNet:
     def add(self, layer):
         self.layers.append(layer)
 
-    def forwardCalculation(self,input):
+    def forwardCalculation(self,input,weight=None):
         for layer in self.layers:
-            output = layer.layerCalculation(input)
+            output = layer.layerCalculation(input, weight)
             input = output
         
         return output
+    
+    def sseCalculation(self, yhat, y):
+        yhat = yhat.apply(lambda x:x[0]) # flatten from list to float
+        sse = sum((yhat - y)**2)
+        return sse
